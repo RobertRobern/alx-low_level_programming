7dea75bf5c65286ffc59a5052d7ebf44c0657a8f
RobertRobern <ndolorobern@gmail.com>
2022-09-08 15:29:45 -0700

    Adding 100-intel

--- 0x00-hello_world/100-main.c
+++ 0x00-hello_world/100-main.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	printf("ALx School");
+	printf("Holberton School ");
 	return (0);
 }
--- 0x00-hello_world/100-main.s
+++ 0x00-hello_world/100-main.s
@@ -2,7 +2,7 @@
 	.text
 	.section	.rodata
 .LC0:
-	.string	"ALx School"
+	.string	"Holberton School "
 	.text
 	.globl	main
 	.type	main, @function
