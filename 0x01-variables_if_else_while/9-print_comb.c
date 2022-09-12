ff053bf569c065b345cb815a2d8f8e232e8b33cc
RobertRobern <ndolorobern@gmail.com>
2022-09-12 09:01:19 -0700

    Print combinations of single-digit numbers separated by comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Print all possible combinations of single-digit numbers
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int num;
+	
+	for (num = 0; num <= 9; num++)
+	{
+		putchar(num + '0');
+		
+		if (num == 9)
+			continue;		
+		
+		putchar(',');		
+		putchar(' ');
+	}
+		
+	putchar('\n');
+		
+	return (0);
+}
