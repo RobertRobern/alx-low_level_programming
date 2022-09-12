0f06bdede4012bd5bd8f934682d21294d0c8a53a
RobertRobern <ndolorobern@gmail.com>
2022-09-12 01:24:29 -0700

    Print lowercase and uppercase alphabet in ascending order

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Print lowercase alphabet and then uppercase
+ * main - Print lowercase and uppercase
  * 		alphabet in ascending order
  *
  * Return: 0 if success
@@ -13,7 +13,7 @@ int main(void)
 	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
 
-	for (letter = 'Z'; letter >= 'A'; letter--)
+	for (letter = 'A'; letter <= 'Z'; letter++)
 		putchar(letter);
 	putchar('\n');
 
