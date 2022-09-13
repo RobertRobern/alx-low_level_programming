88f0e2df34482e2ffd3d211abdb1bf291136736c
RobertRobern <ndolorobern@gmail.com>
2022-09-13 00:38:28 -0700

    Print numbers from 00 to 99

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Print numbers from 00 to 90
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int l = '0';
+	int n = '0';
+
+	while (l <= '9')
+	{
+		while (n <= '9')
+		{
+			if (!(l > n || l == n))
+			{
+				putchar(l);
+				putchar(n);
+				
+				if (l == '8' && n == '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			n++;
+		}
+		n = '0';
+		l++;
+	}
+
+	return (0);
+}
