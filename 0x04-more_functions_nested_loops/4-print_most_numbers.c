307d41cfb64dec639eb9a218373874ab23970e7c
RobertRobern <ndolorobern@gmail.com>
2022-09-16 06:02:10 -0700

    prints the numbers, from 0 to 9, followed by a new line.

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,9 +1,4 @@
-/*
- * File: 4-print_most_numbers.c
- * Auth: Brennan D Baraban
- */
-
-#include "holberton.h"
+#include "main.h"
 
 /**
  * print_most_numbers - Prints the numbers from 0-9 except for 2 and 4.
