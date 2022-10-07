10d2acd5c9670dd75698c93873d8960616b48658
RobertRobern <ndolorobern@gmail.com>
2022-10-07 02:47:06 -0700

    Prints name of executable progam file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,15 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - Prints the programs executable name
+ * @argc: The number of arguments passed
+ * @argv: The array pointer to a string
  *
  * Return: Always 0
  */
-int main(int argc, char **argv)
+int main(__attribute__((unused))int argc, char **argv)
 {
-	_putchar(argv[0]);
+	printf("%s\n", argv[0]);
+
+	return (0);
 }
